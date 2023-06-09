7e708381f8ef982ea2c60ee78563848a0ad4e90b
kimberlyydanij <kimberlydanij@gmail.com>
2023-06-09 15:45:50 +0900

    20230609 | Update

---
+++ test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		double answer = a/b;
+		System.out.println(answer);
+	}
+	}
---
+++ test/T1000.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class T1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		double answer = a/b;
+		System.out.println(answer);
+	}
+	}
