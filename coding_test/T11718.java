e203b170c31d84aed8fdf2f796083988a15b4219
kimberlyydanij <kimberlydanij@gmail.com>
2023-06-18 23:50:22 +0900

    20230618 | Update

---
+++ coding_test/T11718.java
@@ -0,0 +1,15 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T11718 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+		String a = sc.next();
+		System.out.println(a);
+		}
+		sc.close();
+	}
+}
