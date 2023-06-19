9ce071a4b56385ce9a712f5fdebeeb390580d907
kimberlyydanij <kimberlydanij@gmail.com>
2023-06-20 01:59:45 +0900

    20230620 | Update

--- coding_test/T11718.java
+++ coding_test/T11718.java
@@ -7,7 +7,7 @@ public class T11718 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNext()) {
-		String a = sc.next();
+		String a = sc.nextLine();
 		System.out.println(a);
 		}
 		sc.close();
