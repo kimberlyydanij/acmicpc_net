732f07a3768e31850fb18904597d4ce7bf6562a2
kimberlyydanij <kimberlydanij@gmail.com>
2023-06-24 23:48:14 +0900

    20230624 | Update

--- coding_test/T4101.java
+++ coding_test/T4101.java
@@ -6,14 +6,18 @@ public class T4101 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		
-		if(a>b)
+		if(a==0 && b==0) 
+			break;
+		else if(a>b)
 			System.out.println("Yes");
 		else
 			System.out.println("No");
 
 	}
-
+}
 }
