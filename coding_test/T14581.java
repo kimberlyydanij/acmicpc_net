168510001074e02b4f44643dfe0343c48e5e23ba
kimberlyydanij <kimberlydanij@gmail.com>
2023-07-01 23:32:50 +0900

    20230701 | Update

---
+++ coding_test/T14581.java
@@ -0,0 +1,15 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T14581 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		sc.close();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+a+"::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
