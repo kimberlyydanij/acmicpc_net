2ff9f9128133fa0cc33a73ffd4aa6ce020e667ba
kimberlyydanij <kimberlydanij@gmail.com>
2023-07-02 23:49:01 +0900

    20230702 | Update

---
+++ coding_test/T15680.java
@@ -0,0 +1,15 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T15680 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		sc.close();
+		if(a == 0)
+			System.out.println("YONSEI");
+		else System.out.println("Leading the Way to the Future");
+	}
+}
