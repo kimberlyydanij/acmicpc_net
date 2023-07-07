39278096365142ff8fd37d53379494677a67ab40
kimberlyydanij <kimberlydanij@gmail.com>
2023-07-07 23:51:56 +0900

    20230706 | Update

--- coding_test/T15962.java
+++ coding_test/T15962.java
@@ -1,8 +1,10 @@
 package coding_test;
 
-public class T15733 {
+public class T15962 {
 
 	public static void main(String[] args) {
-		System.out.println("I'm Sexy");
+		System.out.println("파이팅!!");
+
 	}
+
 }
