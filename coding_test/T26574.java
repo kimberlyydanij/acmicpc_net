040a006500e2a435d476f2c08258913c8a5c9b6b
kimberlyydanij <kimberlydanij@gmail.com>
2023-07-08 23:53:44 +0900

    20230708 | Update

---
+++ coding_test/T26574.java
@@ -0,0 +1,16 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T26574 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i=0;i<a;i++) {
+			int b = sc.nextInt();
+			System.out.printf("%d %d",b,b);
+		}
+		sc.close();
+	}
+}
